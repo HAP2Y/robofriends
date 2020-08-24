b3ef3fcb730e8c72cf645388ef2bdce42abd7c30
HAP2Y <phappy1422@gmail.com>
2020-08-23 22:21:38 -0400

    uploading my robofriends app

---
+++ src/Robots.js
@@ -0,0 +1,32 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Rudra Parikh',
+        username: 'WiTTED',
+        email: 'witted@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Ved Dave',
+        username: 'RobinTheHoo',
+        email: 'robinthehoo@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Varun Shah',
+        username: 'RR',
+        email: 'tronbtc@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Parth Chavda',
+        username: 'dadvo',
+        email: 'dadvo@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Tushar Agarawal',
+        username: 'tushy',
+        email: '2shark@gmail.com'
+    }
+];
